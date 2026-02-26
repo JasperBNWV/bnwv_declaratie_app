01b1095b313132a48bf131fa9f121d4f3470afeb
Jasper Geertsma <jasper.geertsma@bibliotheeknwveluwe.nl>
2026-02-26 14:45:48 +0100

    Configure webserver in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        baseURL: 'http://localhost:5173/matt-declaratie-helper/',
+        baseURL: 'http://localhost:5173/bnwv_declaratie_app/',
         trace: 'on-first-retry',
     },
     projects: [
@@ -17,4 +17,9 @@ export default defineConfig({
             use: { ...devices['Desktop Chrome'] },
         },
     ],
+    webServer: {
+        command: 'npm run dev',
+        url: 'http://localhost:5173/bnwv_declaratie_app/',
+        reuseExistingServer: !process.env.CI,
+    },
 });
